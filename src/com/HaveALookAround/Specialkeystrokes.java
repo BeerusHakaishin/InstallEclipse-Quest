42de8cfd3347b9c302cc5215abd8c6c9e23d3224
Ismaël Meziani <meziani-i@outlook.fr>
2021-11-28 17:51:03 +0100

    add open terminal shortCut into Specialkeysstrokes class

--- src/com/HaveALookAround/Specialkeystrokes.java
+++ src/com/HaveALookAround/Specialkeystrokes.java
@@ -8,6 +8,7 @@ public class Specialkeystrokes {
 		System.out.println("hello world "
 				+ "if you press CTRL+SHIFT+Y changes to lowercase."
 				+ "CTRL+SHIFT+X changes to UPPERCASE." 
+				+"ALT + CTRL + T for open a terminal"
 				+"For run a class as Java Application just press ALT+SHIFT+X , this command open a"
 				+" litte popUp in bottom right of your screen , you can navigate with the arrow command" 
 				+ "it's a nice shortCut no ? ;) ");
