3dd91d39fff15f01e87fae1c967fafa9b34cf311
Ismaël Meziani <meziani-i@outlook.fr>
2021-11-28 18:05:08 +0100

    add example application

---
+++ src/com/HaveALookAround/methodApplicate.java
@@ -0,0 +1,13 @@
+package com.HaveALookAround;
+
+public class methodApplicate {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String txt = "hello Eclipse Fans";
+		System.out.println(txt.toUpperCase());
+		System.out.println(txt.toLowerCase());
+	}
+	
+}
