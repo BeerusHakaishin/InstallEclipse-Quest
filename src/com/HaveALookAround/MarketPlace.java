23db2196c8a33765be53b0b131366ab89ce07a49
Ismaël Meziani <meziani-i@outlook.fr>
2021-11-28 17:49:33 +0100

    setting up firt java quest

---
+++ src/com/HaveALookAround/MarketPlace.java
@@ -0,0 +1,11 @@
+package com.HaveALookAround;
+
+public class MarketPlace {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) { 
+		System.out.println("You can visit Eclipse Market Place if you want to find some plugins "
+							+"https://marketplace.eclipse.org/");
+		}
+}
---
+++ src/com/HaveALookAround/Specialkeystrokes.java
@@ -0,0 +1,15 @@
+package com.HaveALookAround;
+
+public class Specialkeystrokes {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world "
+				+ "if you press CTRL+SHIFT+Y changes to lowercase."
+				+ "CTRL+SHIFT+X changes to UPPERCASE." 
+				+"For run a class as Java Application just press ALT+SHIFT+X , this command open a"
+				+" litte popUp in bottom right of your screen , you can navigate with the arrow command" 
+				+ "it's a nice shortCut no ? ;) ");
+	}
+}
